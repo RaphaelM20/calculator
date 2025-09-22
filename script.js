39343d82029be8ddc02ad1fa748037f060063b47
Raphael Moreira <43351851+RaphaelM20@users.noreply.github.com>
2025-09-22 18:02:21 -0400

    Updated functions and css
    Wrote four function for add, subtract, multiply, and division
    Added styling to calculator
    Wrote function to store value and display result

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-const calculatorNumbers = document.querySelectorAll('.one-to-nine');
-const operators = document.querySelectorAll('.operator');
-
-let currentInput = "";
-let currentOperand = "";
-let clickedNumbers = [];
-
-//
-
-calculatorNumbers.forEach(number => {
-    number.addEventListener('click', function(){
-        currentInput += number.textContent;
-        console.log('Ans: ', currentInput);
-    })
-})
-
-operators.forEach(operand => {
-    operand.addEventListener('click', function(){
-        if (currentInput !== "")
-            clickedNumbers.push(Number(currentInput));
-            currentInput = "";  
-    })
-    currentOperand = operand.textContent;
-    console.log(currentOperand);
-})
-
-function calculateResult () {
-    let result;
-
-    num1 = clickedNumbers[0];
-    num2 = clickedNumbers[1];
-
-    if (clickedNumbers.length === 2 && currentOperand === '+') 
-        result = num1 + num2
-    elif (clickedNumbers.length === 2 && currentOperand === '-') 
-        result = num1 - num2
-    elif (clickedNumbers.length === 2 && currentOperand === 'x') 
-        result = num1 * num2
-    elif (clickedNumbers.length === 2 && currentOperand === '÷') 
-        result = num1 !== 0 ? num1 / num2 : "Error";
-
-    clickedNumbers = [];
-    currentOperand = "";
-    console.log(result);
+//basic math function
+function addition (num1, num2) {
+    let result = num1 + num2;
     return result;
 }
+
+function subtraction (num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+function multiplication (num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+function division (num1, num2) {
+    if (num2 === 0)
+        return 'Error'
+    let result = num1 / num2;
+    return result;
+}
+
+let num1;
+let num2;
+let operand;
+
+function operate (num1, num2, operand) {
+    switch(operand){
+        case '+':
+            return addition(num1, num2);
+        case '-':
+            return subtraction(num1, num2);
+        case '*':
+            return multiplication(num1, num2);
+        case '÷':
+            return division(num1, num2);
+        default:
+            return 'Invalid Operator';
+    }
+}
+
+//display value
+
+let display = "";
+
+const screen = document.querySelector('.screen');
+
+function updateDisplay() {
+    screen.text = display;
+}
+
+const numberClicked = document.querySelectorAll('.number');
+
+numberClicked.forEach(number => {
+    number.addEventListener('click', function (){
+        display += number.textContent;
+        updateDisplay();
+    })
+})
+
+const operatorClicked = document.querySelectorAll('.operator');
+
+operatorClicked.forEach(op => {
+    op.addEventListener('click', function () {
+        num1 = display;
+        operand = op.textContent;
+        display = '';
+    })
+})
+
+const equalButton = document.querySelector('#equal');
+
+
+equalButton.addEventListener('click', function () {
+    num2 = display;
+    let result = operate(Number(num1), Number(num2), operand);
+    display = result.toString();
+    updateDisplay();
+})
